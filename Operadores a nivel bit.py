d2db292042db3463a78a641f545143132131694b
Juanchi <juanchibgonzalez@gmail.com>
2020-12-26 23:49:35 -0300

    a

---
+++ Operadores a nivel bit.py
@@ -0,0 +1,17 @@
+a = 5 & 4
+print("Resultado del AND", a)
+
+b = 4 | 5
+print("Resultado del OR", b)
+
+c = 4 ^ 5
+print("Resultado del X-OR", c)
+
+d = ~3
+print("Resultado del NOT", d)
+
+izq = 3 << 1
+print("Resultado del Desp izq", izq)
+
+der = 3 >> 1
+print("Resultado del Desp der", der)
---
+++ Operadores logicos y relacionales.py
@@ -0,0 +1,83 @@
+var1 = False
+var2 = True
+print(var1 and var2)
+
+var1 = True
+var2 = True
+print(var1 and var2)
+
+var1 = False
+var2 = False
+print(var1 and var2)
+print("")
+
+var1 = False
+var2 = True
+print(var1 or var2)
+
+var1 = True
+var2 = True
+print(var1 or var2)
+
+var1 = False
+var2 = False
+print(var1 or var2)
+print("")
+
+var1 = True
+print(not var1)
+print("")
+
+num1 = 5
+num2 = 9
+
+print("son iguales:", num1 == num2)
+
+num1 = 9
+num2 = 9
+
+print("son iguales:", num2 == num1)
+print("")
+
+num1 = 5
+num2 = 9
+
+print("son distintos:", num1 != num2)
+
+num1 = 9
+num2 = 9
+
+print("son distintos:", num1 != num2)
+print("")
+
+num1 = 55
+num2 = 9
+
+print("es menor num1 que num2:", num1 < num2)
+
+num1 = 55
+num2 = 99
+
+print("es menor num1 que num2:", num1 < num2)
+print("")
+
+num1 = 55
+num2 = 9
+
+print("es mayor num1 que num2:", num1 > num2)
+
+num1 = 55
+num2 = 99
+
+print("es mayor num1 que num2:", num1 > num2)
+print("")
+
+num1 = 99
+num2 = 99
+
+print("es menor num1 que num2:", num1 <= num2)
+
+num1 = 99
+num2 = 99
+
+print("es menor num1 que num2:", num1 >= num2)

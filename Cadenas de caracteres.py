af6276c8fa03d4eef025aa5597660bf9c4b743bd
Juanchi <juanchibgonzalez@gmail.com>
2020-12-26 23:59:04 -0300

    a

---
+++ Cadenas de caracteres.py
@@ -0,0 +1,22 @@
+cad1="Hello"
+cad2="world"
+print(cad1,cad2)
+
+cad3="Hello "
+print(3*cad3)
+
+long = len("Hello World")
+print(long)
+long = len(cad2)
+print(long)
+long=len("")
+print(long)
+
+cadena="Argentina"
+print("indexacion del caracter 4:", cadena[0])
+
+cadena="Argentina"
+print("indexacion del ultimo caracter:", cadena[-1])
+
+cadena="Argentina"
+print("Acceder a una parte de la cadena", cadena[2:5])

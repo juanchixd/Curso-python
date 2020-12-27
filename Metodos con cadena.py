4e2d806d2ac28d7e7f902f8fce06b083c8c0f92f
Juanchi <juanchibgonzalez@gmail.com>
2020-12-27 17:59:39 -0300

    a

---
+++ Metodos con cadena.py
@@ -0,0 +1,27 @@
+cadena = "hello world"
+result = cadena.capitalize()
+print(result)
+
+cadena = "hello world"
+result = cadena.count("o")
+print(result)
+
+cadena = "hello world"
+result = cadena.endswith("o")
+print("Termina en o?", result)
+
+cadena = "hello world"
+result = cadena.find("z")
+print("Tiene la z?", result)
+
+cadena = "hello world"
+result = cadena.isnumeric()
+print(result)
+
+cadena = "12312"
+result = cadena.isnumeric()
+print(result)
+
+cadena = "HELLO WORLD"
+result = cadena.lower()
+print(result)

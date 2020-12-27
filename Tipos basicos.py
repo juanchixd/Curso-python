4ff51cbb7b15068c4575df27e8aab09ceda14e8f
Juanchi <juanchibgonzalez@gmail.com>
2020-12-26 23:02:22 -0300

    a

---
+++ Tipos basicos.py
@@ -0,0 +1,21 @@
+var1=33
+print(type(var1))
+
+var2=11.22
+print(var2, type(var2))
+
+var3= 3+1j
+print(var3, type(var3))
+
+cad="Hello world"
+print(cad, type(cad))
+
+cad2="Bye my friend"
+print(cad2, type(cad2))
+
+cadN="""Hello
+    Bye"""
+print(cadN)    
+
+valB=True
+print(valB, type(valB))

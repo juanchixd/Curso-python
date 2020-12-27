f0ea7c51f84f15b1ce877398f652ebf370fe8a92
Juanchi <juanchibgonzalez@gmail.com>
2020-12-26 23:25:40 -0300

    a

---
+++ Operadores.py
@@ -0,0 +1,33 @@
+#suma
+total = 32 + 53
+print(total)
+
+totalfloat = 53.8 + 32.7
+print(totalfloat)
+
+#resta
+res = 58-25
+print(res)
+
+#negacion (negativos)
+n = -58
+print(n)
+
+#multiplicacion
+multi = 3 * 6
+print(multi)
+
+#division
+div = 7/2
+print(div)
+
+divE = 7//2
+print(divE)
+
+#exponente
+expo = 2 ** 3
+print(expo)
+
+#modulo
+mod = 7 % 2
+print(mod)
